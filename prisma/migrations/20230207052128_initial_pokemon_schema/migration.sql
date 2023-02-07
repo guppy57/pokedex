9851cb752995c5be910f1164e41c26a5994f815b
Armaan Gupta <armaan@guppy.im>
2023-02-06 23:22:26 -0600

    setup: prisma initialized

---
+++ prisma/migrations/20230207052128_initial_pokemon_schema/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Pokemon" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Pokemon_pkey" PRIMARY KEY ("id")
+);

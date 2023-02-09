95a1606fb2d1f62131cf9153548b1d3d785d0527
Armaan Gupta <armaan@guppy.im>
2023-02-09 00:24:27 -0600

    feat: created get pokemon api route

--- src/pages/api/pokemon/index.ts
+++ src/pages/api/pokemon/index.ts
@@ -1,12 +1,44 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Pokemon, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 type Data = {
-  name: string
-}
+  statusCode: number;
+  message: string;
+  data: Pokemon[];
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // get all pokemon, should have pagination
+  if (req.method === "GET") {
+    // set pagination vairables
+    const limit = req.query.limit ? parseInt(req.query.limit as string) : 20;
+    const offset = req.query.offset ? parseInt(req.query.offset as string) : 0;
+
+    // empty pokemon array
+    let pokemon: Pokemon[] = [];
+
+    try {
+      // get all pokemon based on pagination
+      pokemon = await prisma.pokemon.findMany({
+        skip: offset,
+        take: limit,
+      });
+    } catch (error) {
+      // return error if there is one
+      console.log(error);
+      res
+        .status(500)
+        .json({ statusCode: 500, message: "Internal Server Error", data: [] });
+    }
+
+    // return pokemon
+    res
+      .status(200)
+      .json({ statusCode: 200, message: "Success", data: pokemon });
+  }
 }

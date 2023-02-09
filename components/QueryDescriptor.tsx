bfef69fbaf090f00a0598292d23ea26586c06a3b
Armaan Gupta <armaan@guppy.im>
2023-02-08 18:52:55 -0600

    feat: created base query descriptor

---
+++ components/QueryDescriptor.tsx
@@ -0,0 +1,9 @@
+export default function QueryDescriptor(): JSX.Element {
+  return (
+    <div>
+      <p className="text-sm text-gray-400 text-right pb-2 pr-2">
+        Showing all Pokemon (1279)
+      </p>
+    </div>
+  );
+}

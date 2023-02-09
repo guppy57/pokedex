fd2c44a8e71656368a416a3d9ef99d6ef0ed4e32
Armaan Gupta <armaan@guppy.im>
2023-02-08 18:53:25 -0600

    feat: added all components to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import styles from '@/styles/Home.module.css'
 
 import NavbarComponent from '../../components/Navbar';
 import Splash from '../../components/Splash';
+import PokemonList from '../../components/PokemonList';
+import QueryDescriptor from '../../components/QueryDescriptor';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,13 +15,17 @@ export default function Home(): JSX.Element {
     <div>
       <Head>
         <title>Armaan's Pokedex</title>
-        <meta name="description" content="URL shortening service by Kreative" />
+        <meta name="description" content="A rip-off pokedex for fun." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <NavbarComponent />
         <Splash />
+        <div className="mx-auto max-w-7xl px-6 lg:px-8 pb-12">
+          <QueryDescriptor />
+          <PokemonList />
+        </div>
       </main>
     </div>
   )

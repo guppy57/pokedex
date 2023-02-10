c76e84267ca74106dea7f422d376e39ecb0a7bb9
Armaan Gupta <armaan@guppy.im>
2023-02-10 14:11:23 -0600

    feat: added pokeball icon

--- components/Splash.tsx
+++ components/Splash.tsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Splash(): JSX.Element {
 
   return (
     <div className="mx-auto max-w-7xl px-6 lg:px-8" aria-label="Top">
-      <div id="splash-section" className="sm:pt-24 sm:pb-24 pt-12 pb-12">
+      <div id="splash-section" className="sm:pt-12 sm:pb-12 pt-6 pb-6">
         <div className="">
+          <div className="flex items-center justify-center">
+            <Image 
+              src="/pokeball.png"
+              alt="pokeball"
+              className="pb-6"
+              width={75}
+              height={75}
+            />
+            </div>
           <h1 className="text-black text-center font-bold lg:text-7xl md:text-7xl text-4xl w-full">
             Find every Pokemon, ever.
           </h1>

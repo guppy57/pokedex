5f084a0973535ccc362ce9d92d31994039ccf5ac
Armaan Gupta <armaan@guppy.im>
2023-03-30 12:46:11 -0500

    fix: made toast a placeholder for now

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,7 +1,1 @@
-export default const Toast = ({ message, type }) => {
-  return (
-    <div className={`toast toast-${type}`}>
-      <p>{message}</p>
-    </div>
-  );
-};
+export default function Toast() {}

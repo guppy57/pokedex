9fb7842536f16ae2c1a11d01fae036fe999ed0ee
Armaan Gupta <armaan@guppy.im>
2023-03-31 09:22:26 -0500

    ref: using guards

--- src/pages/api/pokemon/index.ts
+++ src/pages/api/pokemon/index.ts
@@ -9,37 +9,36 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<IResponse>
 ) {
-  // get all pokemon, should have pagination
-  if (req.method === "GET") {
-    // set pagination vairables
-    const limit = req.query.limit ? parseInt(req.query.limit as string) : 20;
-    const page = req.query.page ? parseInt(req.query.page as string) : 1;
+  if (req.method !== "GET") {
+    res.status(404).json({ statusCode: 404, message: "Not Found" });
+  }
+  const limit = req.query.limit ? parseInt(req.query.limit as string) : 20;
+  const page = req.query.page ? parseInt(req.query.page as string) : 1;
 
-    try {
-      // get all pokemon based on pagination
-      const pokemon: Pokemon[] = await prisma.pokemon.findMany({
-        take: limit,
-        skip: page * limit - limit,
-      });
+  try {
+    // get all pokemon based on pagination
+    const pokemon: Pokemon[] = await prisma.pokemon.findMany({
+      take: limit,
+      skip: page * limit - limit,
+    });
 
-      const pokemonCount = await prisma.pokemon.count();
+    const pokemonCount = await prisma.pokemon.count();
 
-      const response: IData = {
-        pokemon: pokemon,
-        totalCount: pokemonCount,
-      };
+    const response: IData = {
+      pokemon: pokemon,
+      totalCount: pokemonCount,
+    };
 
-      // return pokemon
-      // TODO response needs to be moved out of the try block, however we are getting assignment errors
-      res
-        .status(200)
-        .json({ statusCode: 200, message: "Success", data: response });
-    } catch (error) {
-      // return error if there is one
-      console.log(error);
-      res
-        .status(500)
-        .json({ statusCode: 500, message: "Internal Server Error" });
-    }
+    // return pokemon
+    // TODO response needs to be moved out of the try block, however we are getting assignment errors
+    res
+      .status(200)
+      .json({ statusCode: 200, message: "Success", data: response });
+  } catch (error) {
+    // return error if there is one
+    console.log(error);
+    res
+      .status(500)
+      .json({ statusCode: 500, message: "Internal Server Error" });
   }
 }

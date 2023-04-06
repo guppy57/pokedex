a74b132e00ac46c88e3ed713636a915893364175
Armaan Gupta <armaan@guppy.im>
2023-04-05 23:39:19 -0500

    feat: searches when user hits enter

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -12,6 +12,12 @@ export default function SearchBar(): JSX.Element {
     router.push(`/search?q=${encodedQuery}`);
   }
 
+	const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === "Enter") {
+			handleSearch(e);
+		}
+	}
+
   return (
     <form className="mt-4 lg:max-w-screen-md md:max-w-screen-sm sm:mx-auto sm:flex max-w-screen-md" onSubmit={handleSearch}>
       <div className="min-w-0 flex-1">
@@ -27,6 +33,7 @@ export default function SearchBar(): JSX.Element {
           onChange={(e) => setQuery(e.target.value)}
           className="block w-full rounded-md border px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-md focus:border-transparent focus:outline-none focus:ring-1 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-hyper-blue-primary"
           placeholder="Search for a Pokemon..."
+					onKeyUp={handleEnter}
         />
       </div>
       <div className="mt-4 sm:mt-0 sm:ml-3">

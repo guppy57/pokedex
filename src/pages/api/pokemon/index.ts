3a65ec5602ad9c8891637767be4cd2786cc88c13
Armaan Gupta <armaan@guppy.im>
2023-04-05 23:32:00 -0500

    ref: error message more clear

--- src/pages/api/pokemon/index.ts
+++ src/pages/api/pokemon/index.ts
@@ -10,7 +10,7 @@ export default async function handler(
   res: NextApiResponse<IResponse>
 ) {
   if (req.method !== "GET") {
-    res.status(404).json({ statusCode: 404, message: "Not Found" });
+    res.status(404).json({ statusCode: 404, message: "Method not found" });
   }
   const limit = req.query.limit ? parseInt(req.query.limit as string) : 20;
   const page = req.query.page ? parseInt(req.query.page as string) : 1;

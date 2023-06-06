48c03279968ad20e1c9d0f95e0b324f60567948a
Armaan Gupta <armaan@guppy.im>
2023-06-06 14:30:00 -0500

    ref: removed un-needed div

--- components/Splash.tsx
+++ components/Splash.tsx
@@ -5,28 +5,26 @@ export default function Splash(): JSX.Element {
   return (
     <div className="mx-auto max-w-7xl px-6 lg:px-8" aria-label="Top">
       <div id="splash-section" className="sm:pt-12 sm:pb-12 pt-6 pb-6">
-        <div className="">
-          <div className="flex items-center justify-center">
-            <Image 
-              src="/pokeball.png"
-              alt="pokeball"
-              className="w-auto pb-6 md:h-24 h-20"
-              width={100}
-              height={100}
-            />
-            </div>
-          <h1 className="text-black text-center font-bold lg:text-7xl md:text-7xl text-4xl w-full">
-            Find every Pok&eacute;mon, ever.
-          </h1>
-          <div className="flex items-center justify-center">
-            <p className="my-10 text-lg leading-8 text-gray-600 text-center lg:px-18 sm:px-0 lg:w-8/12 w-full">
-              Search the (rip-off) pokedex for any Pok&eacute;mon from any game. Then,
-              view stats like height, weight, type, and more! Why did I make
-              this? Just for fun to be honest.
-            </p>
+        <div className="flex items-center justify-center">
+          <Image
+            src="/pokeball.png"
+            alt="pokeball"
+            className="w-auto pb-6 md:h-24 h-20"
+            width={100}
+            height={100}
+          />
           </div>
-          <SearchBar />
+        <h1 className="text-black text-center font-bold lg:text-7xl md:text-7xl text-4xl w-full">
+          Find every Pok&eacute;mon, ever.
+        </h1>
+        <div className="flex items-center justify-center">
+          <p className="my-10 text-lg leading-8 text-gray-600 text-center lg:px-18 sm:px-0 lg:w-8/12 w-full">
+            Search the (rip-off) pokedex for any Pok&eacute;mon from any game. Then,
+            view stats like height, weight, type, and more! Why did I make
+            this? Just for fun to be honest.
+          </p>
         </div>
+        <SearchBar />
       </div>
     </div>
   );
